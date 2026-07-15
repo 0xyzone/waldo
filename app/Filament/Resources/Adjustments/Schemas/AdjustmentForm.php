9eef7f360c1a2494132f233b7b8dd5aa9a8bcd6d
0xyzone <58982560+0xyzone@users.noreply.github.com>
2026-07-15 12:20:06 +0545

    Update AdjustmentForm.php

--- app/Filament/Resources/Adjustments/Schemas/AdjustmentForm.php
+++ app/Filament/Resources/Adjustments/Schemas/AdjustmentForm.php
@@ -74,7 +74,7 @@ class AdjustmentForm
                                         $set('employee_designation', $employee?->designation?->name);
                                         $set('employee_code', $employee?->employee_code);
                                     })
-                                    ->disabled(fn () => auth()->user()->hasRole('finance')),
+                                    ->disable(fn () => auth()->user()->hasRole('finance')),
 
                                 Select::make('type')
                                     ->label('Adjustment Type')
@@ -85,7 +85,7 @@ class AdjustmentForm
                                     ->native(false)
                                     ->required()
                                     ->default('add')
-                                    ->disabled(fn () => auth()->user()->hasRole('finance')),
+                                    ->disable(fn () => auth()->user()->hasRole('finance')),
 
                                 Select::make('for_month')
                                     ->label('Target Month')
@@ -106,7 +106,7 @@ class AdjustmentForm
                                     ->default(strtolower(now()->format('F')))
                                     ->native(false)
                                     ->required()
-                                    ->disabled(fn () => auth()->user()->hasRole('finance')),
+                                    ->disable(fn () => auth()->user()->hasRole('finance')),
 
                                 Select::make('status')
                                     ->label('Status')
