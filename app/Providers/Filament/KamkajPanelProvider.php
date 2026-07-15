fd54cbb0173e6ceb4888f3ddb2801dc675eb7de8
0xyzone <58982560+0xyzone@users.noreply.github.com>
2026-07-15 11:39:55 +0545

    Update KamkajPanelProvider.php

--- app/Providers/Filament/KamkajPanelProvider.php
+++ app/Providers/Filament/KamkajPanelProvider.php
@@ -51,6 +51,13 @@ class KamkajPanelProvider extends PanelProvider
                     ->group('HR & Admin')
                     ->visible(fn () => auth()->user()->hasRole(['HR'])),
             ])
+            ->navigationGroups([
+                'HR & Admin',
+                'IT',
+                'Finance',
+                'Basic Info',
+                'Filament Shield'
+            ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\Filament\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\Filament\Pages')
             ->pages([
