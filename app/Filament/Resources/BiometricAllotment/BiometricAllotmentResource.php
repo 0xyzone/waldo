5697be0927cc772380375a531899b085974a43ed
0xyzone <58982560+0xyzone@users.noreply.github.com>
2026-07-15 12:16:45 +0545

    Create BiometricAllotmentResource.php

---
+++ app/Filament/Resources/BiometricAllotment/BiometricAllotmentResource.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Filament\Resources\BiometricAllotment;
+
+use App\Filament\Resources\BiometricAllotment\Pages\ListBiometricAllotments;
+use App\Filament\Resources\BiometricAllotment\Schemas\BiometricAllotmentForm;
+use App\Filament\Resources\BiometricAllotment\Tables\BiometricAllotmentsTable;
+use App\Models\BiometricAllotment;
+use Filament\Resources\Resource;
+use Filament\Schemas\Schema;
+use Filament\Support\Icons\Heroicon;
+use Filament\Tables\Table;
+use BackedEnum;
+use UnitEnum;
+
+class BiometricAllotmentResource extends Resource
+{
+    protected static ?string $model = BiometricAllotment::class;
+
+    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedFingerPrint;
+
+    protected static string|BackedEnum|null $activeNavigationIcon = Heroicon::FingerPrint;
+
+    protected static ?int $navigationSort = 4;
+
+    protected static string|UnitEnum|null $navigationGroup = 'IT';
+
+    protected static ?string $recordTitleAttribute = 'name';
+
+    public static function form(Schema $schema): Schema
+    {
+        return BiometricAllotmentForm::configure($schema);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return BiometricAllotmentsTable::configure($table);
+    }
+
+    public static function getRelations(): array
+    {
+        return [];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => ListBiometricAllotments::route('/'),
+        ];
+    }
+}
