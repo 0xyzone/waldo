ed721ee458f54cdace1854c4da5a489127c48a7d
0xyzone <58982560+0xyzone@users.noreply.github.com>
2026-07-22 16:48:55 +0545

    Update BiometricAllotmentsTable.php

--- app/Filament/Resources/BiometricAllotment/Tables/BiometricAllotmentsTable.php
+++ app/Filament/Resources/BiometricAllotment/Tables/BiometricAllotmentsTable.php
@@ -114,7 +114,6 @@ class BiometricAllotmentsTable
 
                             return ! $employee;
                         }
-
                         return false;
                     })
                     ->form([
