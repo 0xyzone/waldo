3863fb06e8914c33251373222e03a780ddb31301
0xyzone <58982560+0xyzone@users.noreply.github.com>
2026-07-31 16:37:32 +0545

    Create TerminatedEmployeePolicy.php

---
+++ app/Policies/TerminatedEmployeePolicy.php
@@ -0,0 +1,75 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Policies;
+
+use Illuminate\Foundation\Auth\User as AuthUser;
+use App\Models\TerminatedEmployee;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class TerminatedEmployeePolicy
+{
+    use HandlesAuthorization;
+    
+    public function viewAny(AuthUser $authUser): bool
+    {
+        return $authUser->can('ViewAny:TerminatedEmployee');
+    }
+
+    public function view(AuthUser $authUser, TerminatedEmployee $terminatedEmployee): bool
+    {
+        return $authUser->can('View:TerminatedEmployee');
+    }
+
+    public function create(AuthUser $authUser): bool
+    {
+        return $authUser->can('Create:TerminatedEmployee');
+    }
+
+    public function update(AuthUser $authUser, TerminatedEmployee $terminatedEmployee): bool
+    {
+        return $authUser->can('Update:TerminatedEmployee');
+    }
+
+    public function delete(AuthUser $authUser, TerminatedEmployee $terminatedEmployee): bool
+    {
+        return $authUser->can('Delete:TerminatedEmployee');
+    }
+
+    public function deleteAny(AuthUser $authUser): bool
+    {
+        return $authUser->can('DeleteAny:TerminatedEmployee');
+    }
+
+    public function restore(AuthUser $authUser, TerminatedEmployee $terminatedEmployee): bool
+    {
+        return $authUser->can('Restore:TerminatedEmployee');
+    }
+
+    public function forceDelete(AuthUser $authUser, TerminatedEmployee $terminatedEmployee): bool
+    {
+        return $authUser->can('ForceDelete:TerminatedEmployee');
+    }
+
+    public function forceDeleteAny(AuthUser $authUser): bool
+    {
+        return $authUser->can('ForceDeleteAny:TerminatedEmployee');
+    }
+
+    public function restoreAny(AuthUser $authUser): bool
+    {
+        return $authUser->can('RestoreAny:TerminatedEmployee');
+    }
+
+    public function replicate(AuthUser $authUser, TerminatedEmployee $terminatedEmployee): bool
+    {
+        return $authUser->can('Replicate:TerminatedEmployee');
+    }
+
+    public function reorder(AuthUser $authUser): bool
+    {
+        return $authUser->can('Reorder:TerminatedEmployee');
+    }
+
+}
